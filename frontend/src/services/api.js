2cc89a4bc95cdd39c39cb28e27492c36d2fc6398
DevStackFlow Team <devstackflowteam@gmail.com>
2026-06-16 16:06:15 +0530

    modified end points

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:5001/api/atm";
+const API = "https://atm-applicatio.onrender.com";
 
 export const signup = (data) => {
   return axios.post(`${API}/signup`, data);
